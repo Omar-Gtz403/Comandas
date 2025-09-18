35bdb83771b706ccc08405bb773e083cc83870f0
Jerry Bemo <gerardo_becmor@outlook.com>
2025-09-18 10:40:22 -0600

    cambios para empaquetar en example

--- apiRestComanda/src/main/java/com/example/demo/DemoApplication.java
+++ apiRestComanda/src/main/java/com/example/demo/DemoApplication.java
@@ -5,7 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.apache.catalina.connector.Connector;
 @SpringBootApplication
 public class DemoApplication extends SpringBootServletInitializer {
 
@@ -17,4 +21,21 @@ public class DemoApplication extends SpringBootServletInitializer {
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         return builder.sources(DemoApplication.class);
     }
+    @Configuration
+    public class TomcatConfig {
+
+        @Bean
+        public WebServerFactoryCustomizer<TomcatServletWebServerFactory> servletContainer() {
+            return factory -> {
+                // Conector HTTP que redirige a HTTPS
+                Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+                connector.setScheme("http");
+                connector.setPort(8080);       // HTTP interno
+                connector.setSecure(false);
+                connector.setRedirectPort(8443); // HTTPS interno
+                factory.addAdditionalTomcatConnectors(connector);
+            };
+        }
+    }
+
 }
